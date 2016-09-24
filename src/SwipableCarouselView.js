737e787a19a13983cf05fb1ed3f00e0fbff9e5da
leMaik <an@maik.tk>
2016-09-24 21:52:28 +0200

    Update react-swipable-views and fix HOC order. See oliviertassinari/react-swipeable-views#156 for more information.

--- src/SwipableCarouselView.js
+++ src/SwipableCarouselView.js
@@ -5,7 +5,7 @@ import bindKeyboard from 'react-swipeable-views/lib/bindKeyboard'
 import SwipeableViews from 'react-swipeable-views'
 import { modulo } from './util'
 
-const VirtualizeAutoPlaySwipeViews = virtualize(autoPlay(bindKeyboard(SwipeableViews)))
+const VirtualizeAutoPlaySwipeViews = autoPlay(bindKeyboard(virtualize(SwipeableViews)))
 
 const carouselSlideRenderer = (children) =>
   ({ index, key }) => React.cloneElement(children[modulo(index, children.length)], { key })
