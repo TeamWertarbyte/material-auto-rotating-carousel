3b640836715d3ceba83a8389f1f27786d7ed4a02
leMaik <an@maik.tk>
2017-02-11 17:30:35 +0100

    Inject tap event before loading stories. Fixes clicking issues after initial storybook load.

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,10 +1,10 @@
 import { configure } from '@kadira/storybook';
 
+const injectTapEventPlugin = require("react-tap-event-plugin");
+injectTapEventPlugin();
+
 function loadStories() {
   require('../stories');
 }
 
 configure(loadStories, module);
-
-const injectTapEventPlugin = require("react-tap-event-plugin");
-injectTapEventPlugin();
